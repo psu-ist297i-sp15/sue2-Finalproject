a51cd16287ef02a2facba3d3afc2ac7f4e13bfb4
Shane Eckenrode <sue2@psu.edu>
2015-04-27 11:01:50 -0400

    Added a table for user comments.

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :comments, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417185643) do
+ActiveRecord::Schema.define(version: 20150427150024) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
